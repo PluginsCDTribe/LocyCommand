4655c4ea20e558dfa25f72a6c91d04caa0c22284
LocyDragon <2424441676@qq.com>
2018-03-03 17:44:24 +0800

    addUtil

--- src/main/java/com/locycommand/LocyCommand.java
+++ src/main/java/com/locycommand/LocyCommand.java
@@ -1,14 +1,18 @@
-package com.pcd.locycommand;
+package com.locycommand;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LocyCommand extends JavaPlugin {
+    public static LocyCommand instance;
     @Override
     public void onEnable() {
-
+        instance = this;
     }
     @Override
     public void onDisable() {
 
+    }
+    public static LocyCommand getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/com/locycommand/util/CommandInvoker.java
@@ -0,0 +1,32 @@
+package com.locycommand.util;
+
+import com.locycommand.LocyCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.PluginCommand;
+import sun.plugin2.main.server.Plugin;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+public class CommandInvoker {
+    public static String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];;
+    public static boolean registerCommand(String cmdName) {
+        try {
+            Object craftServer = Class.forName("org.bukkit.craftbukkit." + version + ".CraftServer").cast(Bukkit.getServer());
+            Field mapField = craftServer.getClass().getField("commandMap");
+            mapField.setAccessible(true);
+            Object commandMap = mapField.get(craftServer);
+            Constructor pluginCommandCon = PluginCommand.class.getConstructor(new Class[]{String.class, Plugin.class});
+            pluginCommandCon.setAccessible(true);
+            Object newCommand = pluginCommandCon.newInstance(cmdName, (org.bukkit.plugin.Plugin)LocyCommand.getInstance());
+            commandMap.getClass().getMethod("register", new Class[]{String.class, Command.class}).invoke(commandMap, new Object[]{"locyitem", newCommand});
+            mapField.set(craftServer, commandMap);
+            return true;
+        } catch (Exception exc) {
+            LocyCommand.getInstance().getLogger().info("插件不支持你服务器的版本");
+            LocyCommand.getInstance().getServer().getPluginManager().disablePlugin(LocyCommand.getInstance());
+            return false;
+        }
+    }
+}
