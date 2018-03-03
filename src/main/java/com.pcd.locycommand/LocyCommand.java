65a44d37651c5471d0085bf286a270b71833a488
LocyDragon <2424441676@qq.com>
2018-03-03 17:24:36 +0800

    build maven

---
+++ src/main/java/com.pcd.locycommand/LocyCommand.java
@@ -0,0 +1,14 @@
+package com.pcd.locycommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LocyCommand extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
